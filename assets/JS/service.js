00e6e7d5ebde56d81774b7a0124d20d922a88a93
HosseinHamta <hosseinandmorty@gmail.com>
2024-01-13 11:49:20 +0330

    form / sevice update

--- assets/JS/service.js
+++ assets/JS/service.js
@@ -17,6 +17,32 @@ const removeFilter = () => {
     container.style.filter = 'none';
 }
 
+const setLines = () => {
+    const underlines = $.querySelectorAll('.service-title .underline');
+    
+    if ($.documentElement.scrollTop >= 450) {
+        underlines[1].classList.remove('exit-underline');
+        underlines[1].classList.add('show-underline');
+    }else {
+        underlines[1].classList.remove('show-underline');
+        underlines[1].classList.add('exit-underline');
+    }
+
+    if ($.documentElement.scrollTop >= 1200) {
+        underlines[2].classList.remove('exit-underline');
+        underlines[2].classList.add('show-underline');
+    }else {
+        underlines[2].classList.remove('show-underline');
+        underlines[2].classList.add('exit-underline');
+    }
+}
+
 window.addEventListener('load', () => {
     removeFilter();
+    setLines();
+})
+
+window.addEventListener('scroll', () => {
+    setLines();
+    console.log($.documentElement.scrollTop);
 })
--- assets/components/requestForm/form.js
+++ assets/components/requestForm/form.js
@@ -42,6 +42,19 @@ integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2
                             </ul>
                         </div>
                     </div>
+                    <div class="checkBoxes">
+                        <div class="form-check form-check-reverse">
+                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="agency">
+                            <label class="form-check-label" for="agency" id="agency-label">
+                                درخواست نمایندگی
+                            </label>
+                        </div>
+                        <div class="form-check form-check-reverse">
+                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="pos-customer">
+                            <label class="form-check-label" for="pos-customer" id="pos-customer-label">
+                            </label>
+                        </div>
+                    </div>
                 </div>
                 <button id="request-btn">ثبت درخواست</button>
                 <small class="msg"> * لطفا اطلاعات رو تکمیل کنید</small>
@@ -75,6 +88,10 @@ class Form extends HTMLElement {
         submitBtn.addEventListener('click', event => {
             event.preventDefault();
             this.checkInfo();
+        })
+
+        window.addEventListener('DOMContentLoaded', () => {
+            this.checkPage()
         })
     }
 
@@ -110,6 +127,21 @@ class Form extends HTMLElement {
                 }else {
                     this.showSuccessMsg("* به زودی با شما تماس خواهیم گرفت")
                 }
+
+                if (menuBtn.textContent.includes('خدمت')){
+                    this.showErrorMsg('* لطفا خدمات مورد نظر را انتخاب نمایید')
+                }else {
+                    if (menuBtn.textContent.includes('شتابی') || menuBtn.textContent.includes('شاپرکی')) {
+                        const agencyCheckbox = this.shadowRoot.getElementById('agency');
+                        const posCustomerCheckbox = this.shadowRoot.getElementById('pos-customer');
+
+                        if (agencyCheckbox.checked || posCustomerCheckbox.checked) {
+                            this.showSuccessMsg("* به زودی با شما تماس خواهیم گرفت")
+                        }else {
+                            this.showErrorMsg('* لطفا خدمت درخواستی خود را انتخاب نمایید')
+                        }
+                    }
+                }
             }
         })
     }
@@ -134,6 +166,50 @@ class Form extends HTMLElement {
         setTimeout(() => {
             msg.classList.remove('success');
         }, 5000);
+    }
+
+    checkPage () {
+        if (location.href.includes('service')) {
+            const productBtn = this.shadowRoot.querySelector('#drop-btn');
+            const formTitle = this.shadowRoot.querySelector('#request-title');
+            const list = this.shadowRoot.querySelector('.dropdown-menu');
+
+            productBtn.textContent = 'خدمت';
+            formTitle.textContent = 'درخواست خدمت';
+            list.innerHTML = '';
+            list.insertAdjacentHTML(`beforeend`, `
+                <li><a class="dropdown-item">خدمات شتابی</a></li>
+                <li><a class="dropdown-item">خدمات شاپرکی</a></li>
+                <li><a class="dropdown-item">پشتیبانی</a></li>
+                <li><a class="dropdown-item">تعمیرات</a></li>
+                <li><a class="dropdown-item">گارانتی</a></li>
+            `);
+
+            const items = this.shadowRoot.querySelectorAll('.dropdown-item');
+
+            items.forEach(item => {
+                item.addEventListener('click', () => {
+                    this.chooseItem(item);
+                    this.checkService()
+                })
+            })
+        }
+    }
+
+    checkService () {
+        const productBtn = this.shadowRoot.querySelector('#drop-btn');
+        const checkBoxes = this.shadowRoot.querySelector('.checkBoxes');
+        const posCustomerLabel = this.shadowRoot.getElementById('pos-customer-label');
+
+        if (productBtn.textContent.includes('شتابی')) {
+            checkBoxes.classList.add('display-flex');
+            posCustomerLabel.textContent = 'درخواست به عنوان متقاضی'
+        }else if (productBtn.textContent.includes('شاپرکی')) {
+            checkBoxes.classList.add('display-flex');
+            posCustomerLabel.textContent = 'فعالسازی POS'
+        }else {
+            checkBoxes.classList.remove('display-flex');
+        }
     }
 }
 
