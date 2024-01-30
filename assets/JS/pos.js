b7a8d2d74479a8486be992a5f9c3528ca94aeee2
MiladHamta <milowinchester6@gmail.com>
2024-01-30 10:45:35 +0330

    update

--- assets/JS/pos.js
+++ assets/JS/pos.js
@@ -24,30 +24,36 @@ const products = [
     },
     {
         id: 1, 
+        title: "ANFU 75",
+        image: 'assets/Images/pos.png',
+        features: [ "شبکه دو سیمکارت 4G", 'دارای وای فای', "پشتیبانی کارتهای مغناطیسی", 'گارانتی یک ساله']
+    },
+    {
+        id: 2, 
         title: "Aisino V72",
         image: 'assets/Images/pos.png',
         features: ["سیستم لینوکس", 'نمایشگر 2.8 اینچی', 'ظرفیت بالای باطری', 'گارانتی طلایی یک ساله']
     },
     {
-        id: 2, 
+        id: 3, 
         title: "Centerm B70",
         image: 'assets/Images/pos.png',
         features: ["سیستم لینوکس", 'ظرفیت بالای باطری', 'پرینتر سریع', 'گارانتی یک ساله']
     },
     {
-        id: 3, 
+        id: 4, 
         title: "Centerm K9",
         image: 'assets/Images/pos.png',
         features: ["سیستم اندرویدی", 'نمایشگر 5.5 اینچی', 'دارای دوربین', 'گارانتی یک ساله']
     },
     {
-        id: 4, 
+        id: 5, 
         title: "Morefun H9",
         image: 'assets/Images/pos.png',
         features: ["سیستم لینوکس", "شبکه دو سیمکارت 4G", 'دارای وای فای', 'گارانتی یک ساله']
     },
     {
-        id: 5, 
+        id: 6, 
         title: "Dspread Trio",
         image: 'assets/Images/pos.png',
         features: ['ظرفیت بالای باطری', 'دارای وای فای', "پشتیبانی کارتهای مغناطیسی", 'گارانتی یک ساله']
