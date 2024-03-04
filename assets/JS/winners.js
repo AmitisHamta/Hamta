455f937e933de86c4a13c60aaf554b1c19229410
MiladHamta <milowinchester6@gmail.com>
2024-03-04 11:30:04 +0330

    update

--- assets/JS/winners.js
+++ assets/JS/winners.js
@@ -10,10 +10,37 @@ window.customElements.define('site-loader', Loader);
 
 const $ = document;
 const container = $.getElementById('container');
+const modalForm = $.getElementById('modal-form');
+const form = $.querySelector('.form');
+const dataBox = $.getElementById('data-box');
+const loaderBox = $.getElementById('loader-box');
+const errMsg = $.querySelector('.err');
+const phoneInput = $.getElementById('phone-input')
+
+const phoneRegex = /^(?:(?:\+|00)98|0)?9\d{9}$/;
 
 const removeFilter = () => {
     container.style.filter = 'none';
 }
+
+const showData = () => {
+    loaderBox.classList.add('display-none');
+    dataBox.classList.add('display-flex')
+}
+
+const showLoader = () => {
+    modalForm.classList.add('display-none');
+    loaderBox.classList.add('display-flex');
+
+    setTimeout(() => {
+        showData();
+    }, 2000);
+}
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    showLoader()
+})
 
 window.addEventListener('load', () => {
     removeFilter();
